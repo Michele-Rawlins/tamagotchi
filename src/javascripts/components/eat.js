b9df132cc432549f1cfa6e958701fb36494034f7
Michele Rawlins <shellbop@att.net>
2020-03-08 21:19:14 -0500

    commented code

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -34,4 +34,25 @@ const eatBarButtons = () => {
   document.getElementById('healthy').addEventListener(' click', feedMe);
 };
 
+// const createHouseButton = (house) => {
+  // const domString = `
+    // <button id="${house.id}" class="house-button house-button-${house.name.toLowerCase()}">
+      // <div>${house.name}</div>
+      // <img src="${house.picture}">
+    // </button>
+  // `;
+  // return domString;
+// };
+// 
+// const buttonEventFunction = (e) => {
+  // const houseId = e.target.closest('button').id;
+  // const students = studentData.getStudents();
+  // const filteredStudents = students.filter((x) => x.houseId === houseId);
+  // studentList.createStudentList(filteredStudents);
+// };
+// 
+export default { createHouseButton, buttonEventFunction };
+
+
+
 export default { eatBarButtons };
