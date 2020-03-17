fa9b143993e8102f5ef514f834aa0b82e291ad40
Michele Rawlins <shellbop@att.net>
2020-03-16 23:48:20 -0500

    added status bar

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -14,45 +14,33 @@ const feedEvents = () => {
     feedButtons[i].addEventListener('click', feedMe);
   }
 };
+const progressBar = () => {
+  const getComputedStyle = '';
+  const i = document.getElementsByClassName('progress-bar')[0];
+  setInterval(() => {
+    const computedStyle = getComputedStyle(i);
+    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 100;
+    progressBar.style.setProperty('--width', width + 1);
+  }, 5);
+};
 
 const eatBarButtons = () => {
   let domString = '';
   domString += '<div id = "eatQuad" >';
   domString += '<h2>Eat</h2>';
-  domString += '<div id="myProgress">';
-  domString += '<div id="myBar"></div>';
   domString += '<div class = "row">';
   domString += '<button class="button" id = "unhealthy"><i class="fas fa-bacon"></i>Unhealthy</button>';
   domString += '<button class="button" id = "healthy"><i class="fas fa-apple-alt"></i>Healthy</button>';
   domString += '</div>';
   domString += '</div>';
-  domString += '<div>';
-
+  domString += '<div class="progress-bar" data-label = "Feeding" style="--width: 100">';
+  domString += '</div>';
 
   utils.printToDom('eat', domString);
   feedEvents();
+  progressBar();
   document.getElementById('unhealthy').addEventListener('click', feedMe);
   document.getElementById('healthy').addEventListener(' click', feedMe);
 };
-
-// const createHouseButton = (house) => {
-// const domString = `
-// <button id="${house.id}" class="house-button house-button-${house.name.toLowerCase()}">
-// <div>${house.name}</div>
-// <img src="${house.picture}">
-// </button>
-// `;
-// return domString;
-// };
-
-// const buttonEventFunction = (e) => {
-// const houseId = e.target.closest('button').id;
-// const students = studentData.getStudents();
-// const filteredStudents = students.filter((x) => x.houseId === houseId);
-// studentList.createStudentList(filteredStudents);
-// };
-
-// export default { createHouseButton, buttonEventFunction };
-
 
 export default { eatBarButtons };
